545d6864f74ee729c78ded1734f706ccbcb4fae8
Hanitra <hvtinarak@gmail.com>
2026-03-10 14:13:57 +0800

    Addition de je me nomme par Hanitra

--- age.py
+++ age.py
@@ -2,5 +2,5 @@ age = 30
 nom = "Hanitra"
 print(f"Je m'appelle {nom} et j'ai {age} ans")
 age = age + 10 + 5
-print(f"Je m'appelle {nom} et j'ai {age} ans maintenant")
+print(f"Je me nomme {nom} et j'ai {age} ans maintenant")
 print(f"Je m'appelle {nom} et j'ai {age+1} ans maintenant")
