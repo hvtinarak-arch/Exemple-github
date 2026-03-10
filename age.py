d0d8d689b5a0b965032406ff32ef1f3b6d125d68
Hanitra <hvtinarak@gmail.com>
2026-03-10 14:44:09 +0800

    Une nouvelle mise à jour

--- age.py
+++ age.py
@@ -3,4 +3,4 @@ nom = "Tina Hanitra"
 print(f"Je m'appelle {nom} et j'ai {age} ans")
 age = age + 10 + 5
 print(f"Je me nomme {nom} et j'ai {age} ans maintenant")
-print(f"Je m'appelle {nom} et j'ai {age+1} ans maintenant")
+print("Je suis ravi d'utilisé GitHub")
