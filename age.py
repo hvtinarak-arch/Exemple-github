3f2785947ca11abd56bd37ebc6f132d9eeeacf2a
Tina <hvtinarak@gmail.com>
2026-03-10 13:55:13 +0800

    Ajout de nouvelle ligne par Tina

--- age.py
+++ age.py
@@ -3,3 +3,4 @@ nom = "Hanitra"
 print(f"Je m'appelle {nom} et j'ai {age} ans")
 age = age + 10 + 5
 print(f"Je m'appelle {nom} et j'ai {age} ans maintenant")
+print(f"Je m'appelle {nom} et j'ai {age+1} ans maintenant")
